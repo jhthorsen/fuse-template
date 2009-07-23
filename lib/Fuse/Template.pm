d770c811b8d6f718c1a65d869a965c13df51b3ed
Jan Henning Thorsen <git@flodhest.net>
2009-07-23 19:03:33 +0200

    add run(), find_file() and log()

--- lib/Fuse/Template.pm
+++ lib/Fuse/Template.pm
@@ -11,6 +11,107 @@ Fuse::Template - Mount a template dir
 =cut
 
 use Moose;
+use threads;
+use threads::shared;
+
+require 'syscall.ph'; # for SYS_mknod and SYS_lchown
+
+with qw/Fuse::Template::Sys/;
+
+=head1 ATTRIBUTES
+
+=head2 debug
+
+ $bool = $self->debug;
+
+=cut
+
+has debug => (
+    is => 'ro',
+    isa => 'Bool',
+    default => 0,
+);
+
+=head2 mountpoint
+
+ $path = $self->mountpoint;
+
+Required.
+
+=cut
+
+has mountpoint => (
+    is => 'ro',
+    isa => 'Str',
+    required => 1,
+);
+
+=head2 mountopts
+
+ $str = $self->mountopts;
+
+Default: "allow_other".
+
+=cut
+
+has mountopts => (
+    is => 'ro',
+    isa => 'Str',
+    default => 'allow_other',
+);
+
+=head1 METHODS
+
+=head2 run
+
+=cut
+
+sub run {
+    my $self = shift;
+    my %callbacks;
+
+    for my $method (Fuse::Template::Sys->meta->get_method_list) {
+        $callbacks{$method} = sub { $self->$method(@_) };
+    }
+
+    Fuse::main(
+        mountpoint => $self->mountpoint,
+        debug      => $self->debug,
+        mountopts  => $self->mountopts,
+        threaded   => 1,
+        %callbacks,
+    );
+
+    return 0;
+}
+
+=head2 find_file
+
+ $real_path = $self->find_file($virtual_path);
+
+Returns path to the actual template file, from C<$virtual_path>.
+
+=cut
+
+sub find_file {
+    my $self  = shift;
+    my $vfile = shift;
+
+    # ...
+
+    return "";
+}
+
+=head2 log
+
+ $bool = $self->log($level, $format, @args);
+
+=cut
+
+sub log {
+    my $self = shift;
+    warn "@_";
+}
 
 =head1 AUTHOR
 

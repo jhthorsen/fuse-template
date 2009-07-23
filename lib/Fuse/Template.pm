4d28649ebee71ab597360cac232939241fbb7131
Jan Henning Thorsen <git@flodhest.net>
2009-07-23 10:44:27 +0200

    initial structure

---
+++ lib/Fuse/Template.pm
@@ -0,0 +1,21 @@
+package Fuse::Template;
+
+=head1 NAME
+
+Fuse::Template - Mount a template dir
+
+=head1 SYNOPSIS
+
+=head1 DESCRIPTION
+
+=cut
+
+use Moose;
+
+=head1 AUTHOR
+
+Jan Henning Thorsen
+
+=cut
+
+1;
